13dae737d7a5e8e543b9407f8d18720cfb01b56e
Brandon <brandontetrick@gmail.com>
2020-11-12 10:16:31 -0700

    Basic Error Handling

--- tweets/views.py
+++ tweets/views.py
@@ -21,12 +21,17 @@ def tweet_create_view(request, *args, **kwargs):
         obj =  form.save(commit=False)
         # do other form related logic
         obj.save()
+
         if request.is_ajax(): 
             return JsonResponse(obj.serialize(), status=201) #201 == created items
 
         if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS): 
             return redirect(next_url)
         form = TweetForm()
+    if form.errors:
+        if request.is_ajax(): 
+            return JsonResponse(form.errors, status=400)
+    
     return render(request, 'components/form.html', context={"form": form})
 
 
