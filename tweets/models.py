a8364b79284851f3a78b94cba951fae9d0764309
Brandon <brandontetrick@gmail.com>
2020-11-18 11:12:25 -0700

    Added Action View for Likes & Retweets

--- tweets/models.py
+++ tweets/models.py
@@ -4,13 +4,19 @@ from django.db import models
 
 User = settings.AUTH_USER_MODEL
 
-# Create your models here.
+
+class TweetLike(models.Model): 
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey("Tweet", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
 class Tweet (models.Model):
-    # id = models.AutoField(primary_key=True) 
+    # id = models.AutoField(primary_key=True) DEFAULT
     user = models.ForeignKey(User, on_delete=models.CASCADE) # Foreign Key = one user can have many tweets (instead of many tweets can have many users)
+    likes = models.ManyToManyField(User, related_name='tweet_user', blank=True, through=TweetLike) # Store indivdual users into likes. Many to Many = 1 Tweet could have many users. Many users can have many tweets 
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     #def __str__(self): 
         #return self.content
