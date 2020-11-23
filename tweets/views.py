1961a979507095649cffc139c7043294f26c0414
Brandon <brandontetrick@gmail.com>
2020-11-23 13:44:07 -0700

    Tests Setup

--- tweets/views.py
+++ tweets/views.py
@@ -87,6 +87,8 @@ def tweet_action_view(request, *args, **kwargs):
             return Response(serializer.data, status=200)
         elif action == "unlike": 
             obj.likes.remove(request.user)
+            serializer = TweetSerializer(obj)
+            return Response(serializer.data, status=200)
         elif action == "retweet": 
             new_tweet = Tweet.objects.create(
                 user=request.user, 
@@ -94,7 +96,7 @@ def tweet_action_view(request, *args, **kwargs):
                 content=content,
             )
             serializer = TweetSerializer(new_tweet)
-            return Response(serializer.data, status=200)
+            return Response(serializer.data, status=201)
     return Response({}, status=200)
 
 def tweet_create_view_pure_django(request, *args, **kwargs):
