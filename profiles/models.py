78aaba54f09f358c75597d3f177ee9360900a718
Brandon <brandontetrick@gmail.com>
2021-02-22 09:29:20 -0700

    Added Followers & Test Case

--- profiles/models.py
+++ profiles/models.py
@@ -7,11 +7,25 @@ from django.db.models.signals import post_save
 
 User = settings.AUTH_USER_MODEL
 
+class FollowerRelation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey("Profile", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
 class Profile(models.Model): 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     location = models.CharField(max_length=220, null=True, blank=True)
     bio = models.TextField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    followers = models.ManyToManyField(User, related_name='following', blank=True)
 
+    '''
+    Why is related name called "following" instead of "followers"
+    project_obj = Projects.objects.first()
+    project_obj.follwers.all() -> All users following this profile
+    user.following.all() -> All users I follow
+    '''
 
 # Signal Trigger Function - Once an account is made it will create a profile user. 
 def user_did_save(sender, instance, created, *args, **kwargs):
