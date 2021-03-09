f3439519a1f799b0b0c57d4f19d2cc3a90b6c92f
Brandon <brandontetrick@gmail.com>
2021-03-09 10:34:43 -0700

    Feed Pagination

--- tweets/api/views.py
+++ tweets/api/views.py
@@ -6,6 +6,7 @@ from django.utils.http import is_safe_url
 
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication, BasicAuthentication
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from ..forms import TweetForm
@@ -30,20 +31,27 @@ def tweet_create_view(request, *args, **kwargs):
         return Response(serializer.data, status=201)
     return Response({}, status=400)
 
+
+def get_paginated_queryset_response(qs, request): 
+    paginator = PageNumberPagination()
+    paginator.page_size = 20 
+    paginated_qs = paginator.paginate_queryset(qs, request)
+    serializer = TweetSerializer(paginated_qs, many=True)
+    return paginator.get_paginated_response(serializer.data)
+
 '''
-TWEET FEED VIEW - Gets a List of All Tweets from Followed Users
+TWEET FEED VIEW - List of All Tweets from Followed Users
 Model Manager(feed) - Query Set Filters Are In tweets.models.
-'''
+''' 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def tweet_feed_view(request, *args, **kwargs): 
     user = request.user
     qs = Tweet.objects.feed(user)
-    serializer = TweetSerializer(qs, many=True)
-    return Response(serializer.data)
+    return get_paginated_queryset_response(qs, request)
 
 '''
-TWEET LIST VIEW - Gets a List of All Tweets
+TWEET LIST VIEW - List of All Tweets
 Model Manager(by_username) - Query Set Filters Are In tweets.models. 
 '''
 @api_view(['GET'])
@@ -52,8 +60,7 @@ def tweet_list_view(request, *args, **kwargs):
     username = request.GET.get('username') # ?username=brandon
     if username != None: 
         qs = qs.by_username(username)
-    serializer = TweetSerializer(qs, many=True)
-    return Response(serializer.data)
+    return get_paginated_queryset_response(qs, request)
 
 @api_view(['GET'])
 def tweet_detail_view(request, tweet_id, *args, **kwargs): 
